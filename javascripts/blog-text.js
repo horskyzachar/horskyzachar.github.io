a82702321962a499603e09a81bf8e39e4e6fa353
Jozef Harinek <j.harinek@gmail.com>
2015-09-07 21:29:54 +0200

    Change blog title size

--- javascripts/blog-text.js
+++ javascripts/blog-text.js
@@ -1,6 +1,8 @@
 var adjustBlogTitle = (function() {
+  var padding = parseInt($('.blog-title').css('padding-left'), 10);
+
   var width = $('.blog-title').width()/2;
-  $('.blog-title').css('margin-left', -(width+107));
+  $('.blog-title').css('margin-left', -(width + padding + 7));
 });
 
 $(document).ready(function(){
